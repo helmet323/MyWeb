9bd2e59621aec19ebde2dfa7104d2899a41c446f
Tommy Su <su.tommy23@gmail.com>
2023-02-04 23:19:42 -0500

    Initial commit

---
+++ my-web/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navbar.css"
+
+function Navbar() {
+    return (
+        <div className="navbar__container">
+            <div className="navbar__item">About me</div>
+            <div className="navbar__item">Portfolio</div>
+            <div className="navbar__item">Contact</div>
+        </div>
+    );
+}
+
+export default Navbar;
